b18fe18100779954faf4a3a68b5ca3cca6b8b99d
nul00000000 <fireadam110@gmail.com>
2017-08-15 19:16:17 -0400

    Update mane.js

--- mane.js
+++ mane.js
@@ -2,7 +2,7 @@ var img = document.querySelector("img");
 img.onclick = function() {
   alert("LET ME OUT WUT DID I DOOOOOOOO AHHHHHHHHH!!!!!");
   if(img.getAttribute("src") === "https://raw.githubusercontent.com/RainbowTaco/whytho/master/prisonboi.png") {
-    img.setAttribute("src","/angryjail.png");
+    img.setAttribute("src","https://raw.githubusercontent.com/RainbowTaco/whytho/master/angryjail.png");
   }
   else {
     img.setAttribute("src","https://raw.githubusercontent.com/RainbowTaco/whytho/master/prisonboi.png");
@@ -10,6 +10,6 @@ img.onclick = function() {
 }
 document.querySelector("button").onclick = function() {
   var name = prompt("ENTrE YuOr UuseR nAme:");
-  var password = prompt("ENTRE ThE PAASsWOrd:");
-  alert("HAhAH WeE DonT HavE AcCOuNTs, " + name + ", BuT NoW wEe Noe YOuer passWWoRD Iz " + password + " buTT IST  WuS NT SAVEd");
+  var passw = prompt("ENTRE ThE PAASsWOrd:");
+  alert("HAhAH WeE DonT HavE AcCOuNTs, " + name + ", BuT NoW wEe Noe YOuer passWWoRD Iz " + passw + " buTT I DoDnT REmebeR iT");
 }
