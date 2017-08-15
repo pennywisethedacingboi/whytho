b0ec79016b0d9e1a8fc133d5e0a2cc31a0fab1b9
nul00000000 <fireadam110@gmail.com>
2017-08-15 19:36:32 -0400

    Add files via upload

---
+++ mane.js
@@ -0,0 +1,15 @@
+var img = document.querySelector("img");
+img.onclick = function() {
+  alert("LET ME OUT WUT DID I DOOOOOOOO AHHHHHHHHH!!!!!");
+  if(img.getAttribute("src") === "prisonboi.png") {
+    img.setAttribute("src","angryjail.png");
+  }
+  else {
+    img.setAttribute("src","prisonboi.png");
+  }
+}
+document.querySelector("button").onclick = function() {
+  var name = prompt("ENTrE YuOr UuseR nAme:");
+  var password = prompt("ENTRE ThE PAASsWOrd:");
+  alert("HAhAH WeE DonT HavE AcCOuNTs, " + name + ", BuT NoW wEe Noe YOuer passWWoRD Iz " + password + " buTT IST  WuS NT SAVEd");
+}
